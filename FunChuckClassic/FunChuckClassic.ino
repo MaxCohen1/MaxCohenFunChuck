371c8502f2f8ecb29886842630bb4833deb9feb6
MaxCohen1 <macocohen@gmail.com>
2022-01-31 12:18:04 -0500

    Created folders, added classic buttons
    The Classic Controller functionality is still in development

--- FunChuckClassic/FunChuckClassic.ino
+++ FunChuckClassic/FunChuckClassic.ino
@@ -3,15 +3,21 @@ Accessory nunchuck1;
 
 int lastLeftJoyX = 128;
 int leftJoyX = 128;
-
 int lastLeftJoyY = 128;
 int leftJoyY = 128;
-
 int lastRightJoyX = 128;
 int rightJoyX = 128;
-
 int lastRightJoyY = 128;
 int rightJoyY = 128;
+
+int lastXButton = 0;
+int xButton = 0;
+int lastYButton = 0;
+int yButton = 0;
+int lastAButton = 0;
+int aButton = 0;
+int lastBButton = 0;
+int bButton = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -25,6 +31,10 @@ void loop() {
   checkLeftY();
   checkRightX();
   checkRightY();
+  checkXButton();
+  checkYButton();
+  checkAButton();
+  checkBButton();
 }
 
 void checkLeftX() {
@@ -63,4 +73,46 @@ void checkRightY() {
   lastRightJoyY = rightJoyY;
 }
 
+void checkXButton() {
+  xButton = nunchuck1.values[8];
+  if (xButton == 255 and xButton != lastXButton) {
+    Serial.println("X Button On");
+  }
+  lastXButton = xButton;
+}
+
+void checkYButton() {
+  yButton = nunchuck1.values[9];
+  if (yButton == 255 and yButton != lastYButton) {
+    Serial.println("Y Button On");
+  }
+  lastYButton = yButton;
+}
+
+void checkAButton() {
+  aButton = nunchuck1.values[12];
+  if (aButton == 255 and aButton != lastAButton) {
+    Serial.println("A Button On");
+  }
+  lastAButton = aButton;
+}
+
+void checkBButton() {
+  bButton = nunchuck1.values[13];
+  if (bButton == 255 and bButton != lastBButton) {
+    Serial.println("B Button On");
+  }
+  lastBButton = bButton;
+}
+
 // values[0]=JoyXLeft
+// values[1]=JoyYLeft
+// values[2]=JoyXRight
+// values[3]=JoyYRight
+// values[4]=Nothing
+// values[5]=Nothing
+// values[6]=Broken D Pad (?)
+// values[7]=Broken D Pad (?)
+// values[8]=X Button
+// values[9]=Y Button
+// values[10]=ZL
